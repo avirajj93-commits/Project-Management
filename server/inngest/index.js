43e1a7f4367e092b8173a54cfc1819cf077029ba
avirajj93-commits <avirajsingh25.2005@gmail.com>
2026-02-14 14:45:59 +0530

    inggest issue fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,8 +1,8 @@
-import { Inngest } from "inngest";
+import { inngest } from "inngest";
 import prisma from "../configs/prisma.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "project management" });
+export const inngest = new inngest({ id: "project management" });
 
 const syncUserCreation = inngest.createFunction(
     { id: 'sync-user-from-clerk' },
