3e6bba4f603de4d589155d544471eb0df4fe2c75
Avirajj <avirajj93@gmail.com>
2026-04-18 00:15:14 +0530

    backend complete

---
+++ server/controllers/commentController.js
@@ -0,0 +1,59 @@
+// ADD COMMENT
+
+export const addComment = async (req, res) => {
+    try {
+        const {userId} = await req.auth();
+        const{content, taskId} = req.body;
+
+        //check is user is project member
+        const task = await prisma.task.findUnique({
+            where: {id: taskId},
+           
+        })  
+       const project = await prisma.project.findUnique({
+        where: {id: task.projectId},
+        include: {members:{include:{user:true}}}
+       })   
+         if(!project){  
+            return res.status(404).json({ message: "Project not found" });
+         }
+        
+         const member = project.members.find((member) => member.userId === userId);
+         if(!member){
+            return res.status(403).json({ message: "You are not a member of this project" });
+         }
+
+         const comment = await prisma.comment.create({
+            data: {
+                content,
+                userId,
+                taskId
+            },
+            include: {user:true}    
+        });
+res.json({comment})
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.code || error.message});
+    }   
+}
+
+
+//GET COMMENTS FOR A TASK
+export const getComments = async (req, res) => {
+    try {
+        
+        const {taskId} = req.params;
+        const comments = await prisma.comment.findMany({
+            where: {taskId},
+            include: {user:true}
+        })
+        res.json({comments})
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.code || error.message});
+    }       
+}
--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ import { inngest, functions } from "./inngest/index.js";
 import workspaceRouter from './routes/workspaceRoutes.js';
 import { protect } from './middlewares/authMiddleware.js';
 import webhookRoutes from './routes/webhookRoutes.js';
+import projectRouter from './routes/projectRoutes.js';
+import taskRouter from './routes/taskRoutes.js';
+import commentRouter from './routes/commentRoutes.js';
 const app = express();   // ✅ FIRST create app
 
 app.use(express.json()); // ✅ then middlewares
@@ -36,8 +39,10 @@ app.use(
 );
 
 //ROUTES
-app.use('/api/workspaces',protect, workspaceRouter);
-
+app.use("/api/workspaces", protect, workspaceRouter);
+app.use("/api/projects", protect, projectRouter);
+app.use("/api/tasks", protect, taskRouter);
+app.use("/api/comments", protect, commentRouter);
 
 
 const PORT = process.env.PORT || 5000;
