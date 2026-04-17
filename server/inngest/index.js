c5e520af257eb15fde10b6a21b44352d1858ba3a
Avirajj <avirajj93@gmail.com>
2026-04-18 00:46:44 +0530

    fix nodemailer import

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import prisma from "../configs/prisma.js";
-import sendEmail from "../configs/nodeMailer.js";
+import sendEmail from "../configs/nodemailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "project management" });
